8c44e844ee4a1977d716e820f59039f15f3b20d6
Pooya <pooya.dev01@gmail.com>
2026-08-24 18:47:35 +0330

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.cs
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> Qp = new Queue<TreeNode>();
+        Queue<TreeNode> Qq = new Queue<TreeNode>();
+
+        Qp.Enqueue(p);
+        Qq.Enqueue(q);
+
+        while (Qp.Count > 0 && Qq.Count > 0) {
+            for (int i = Qp.Count; i > 0; i--) {
+                TreeNode nodeP = Qp.Dequeue();
+                TreeNode nodeQ = Qq.Dequeue();
+
+                if (nodeP == null && nodeQ == null)
+                    continue;
+
+                if ((nodeP == null && nodeQ != null) || (nodeP != null && nodeQ == null) ||
+                    (nodeP.val != nodeQ.val)) {
+                    return false;
+                }
+
+                Qp.Enqueue(nodeP.right);
+                Qp.Enqueue(nodeP.left);
+                Qq.Enqueue(nodeQ.right);
+                Qq.Enqueue(nodeQ.left);
+            }
+        }
+        return true;
+    }
+}
